30d2f3aa297bd47c78ccc199680e81e3ef887000
CarlosDHH <charlydhh18@gmail.com>
2024-03-10 13:54:23 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/Components/Formulario.js
    	new file:   src/assets/Bienvenido.png
    	new file:   src/assets/bienvenido.jpg
    	new file:   src/assets/fondo.jpg
    	new file:   src/assets/fondoTodo.png
    
     Changes not staged for commit:
    	modified:   src/App.js
    	modified:   src/Components/Formulario.js
    
     Untracked files:
    	src/Components/FrmLogin.js
    	src/Components/FrmRecuperar.js
    	src/Components/FrmRegistro.js

--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,73 +1,83 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Importar desde react-router-dom para la navegación
+import imgFondo from '../assets/bienvenido.jpg'
 
-const Formulario = () => {
-  const [nombre, setNombre] = useState("");
-  const [apellido, setApellido] = useState("");
-  const [usuario, setUsuario] = useState("");
-  const [correo, setCorreo] = useState("");
-  const [contrasena, setContrasena] = useState("");
-  const [confirmaContrasena, setConfirmarContrasena] = useState("");
-  const [telefono, setTelefono] = useState("");
-
-  const handleRegister = (e) => {
-    e.preventDefault();
-    console.log("Registro:", {
-      nombre,
-      apellido,
-      usuario,
-      correo,
-      contrasena,
-      confirmaContrasena,
-      telefono,
-    });
-  };
-
-  return (
-    <div style={styles.container}>
-      <div style={styles.imgArea}>
-      </div>
-
-      <div style={styles.formArea}>
-        <div style={styles.formContainer}>
-          <h2 style={styles.title}>Registro</h2>
-          <form onSubmit={handleRegister}>
-            <label style={styles.label}>Nombre</label>
-            <input style={styles.input} type="text" placeholder="Ingrese su Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-                  
-            <label style={styles.label}>Apellido</label>
-            <input style={styles.input} type="text" placeholder="Apellido " value={apellido} onChange={(e) => setApellido(e.target.value)} />
-            
-            <label style={styles.label}>Usuario</label>
-            <input style={styles.input} type="text" placeholder="Usuario" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
-            
-            <label style={styles.label}>Correo</label>
-            <input style={styles.input} type="email" placeholder="Correo" value={correo} onChange={(e) => setCorreo(e.target.value)} />
-            
-            <label style={styles.label}>Contraseña</label>
-            <input style={styles.input} type="password" placeholder="Contraseña" value={contrasena} onChange={(e) => setContrasena(e.target.value)} />
-            
-            <label style={styles.label}>Confirmar Contraseña</label>
-            <input style={styles.input} type="password" placeholder="Confirmar contraseña" value={confirmaContrasena} onChange={(e) => setConfirmarContrasena(e.target.value)} />
-            
-            <label style={styles.label}>Teléfono</label>
-            <input style={styles.input} type="tel" placeholder="Teléfono" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
-            
-            <button style={styles.registerButton} type="submit">¡Regístrate ahora!</button>
-          </form>
-          {/* Enlace a la página de login usando React Router */}
-          <Link to="/login" style={styles.loginButtonText}>¿Ya tengo Cuenta?</Link>
+  const Formulario = () => {
+    const [nombre, setNombre] = useState("");
+    const [apellido, setApellido] = useState("");
+    const [usuario, setUsuario] = useState("");
+    const [correo, setCorreo] = useState("");
+    const [contrasena, setContrasena] = useState("");
+    const [confirmaContrasena, setConfirmarContrasena] = useState("");
+    const [telefono, setTelefono] = useState("");
+  
+    const handleRegister = (e) => {
+      e.preventDefault();
+      console.log("Registro:", {
+        nombre,
+        apellido,
+        usuario,
+        correo,
+        contrasena,
+        confirmaContrasena,
+        telefono,
+      });
+    };
+  
+    return (
+      <div style={styles.container}>
+        <div style={styles.imgArea}>
+          <img style={styles.ImagenFondo} src={imgFondo} alt="Fondo"/>
+  
         </div>
+  
+        <div style={styles.formArea}>
+          <div style={styles.formContainer}>
+            <h2 style={styles.title}>Registro</h2>
+            <form onSubmit={handleRegister}>
+  
+              <div style={styles.cincuenta}>
+                <div style={styles.inCincuenta}>
+                  <label style={styles.label}>Nombre</label>
+                  <input style={styles.input} type="text" placeholder="Ingrese su Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                </div>
+                <div style={styles.inCincuenta}>
+                  <label style={styles.label}>Apellido</label>
+                  <input style={styles.input} type="text" placeholder="Apellido " value={apellido} onChange={(e) => setApellido(e.target.value)} />
+                </div>
+              </div>
+              
+              <label style={styles.label}>Usuario</label>
+              <input style={styles.input} type="text" placeholder="Usuario" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
+              
+              <label style={styles.label}>Correo</label>
+              <input style={styles.input} type="email" placeholder="Correo" value={correo} onChange={(e) => setCorreo(e.target.value)} />
+              
+              <label style={styles.label}>Contraseña</label>
+              <input style={styles.input} type="password" placeholder="Contraseña" value={contrasena} onChange={(e) => setContrasena(e.target.value)} />
+              
+              <label style={styles.label}>Confirmar Contraseña</label>
+              <input style={styles.input} type="password" placeholder="Confirmar contraseña" value={confirmaContrasena} onChange={(e) => setConfirmarContrasena(e.target.value)} />
+              
+              <label style={styles.label}>Teléfono</label>
+              <input style={styles.input} type="tel" placeholder="Teléfono" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+              
+              <button style={styles.registerButton} type="submit">¡Regístrate ahora!</button>
+            </form>
+            {/* Enlace a la página de login usando React Router */}
+            <Link to="/login" style={styles.loginButtonText}>¿Ya tengo Cuenta?</Link>
+          </div>
+        </div>
+  
+        
       </div>
-
-      
-    </div>
-  );
-};
+    );
+  };
 
 
 const styles = {
   container: {
+    backgroundColor:'#daebff',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -75,25 +85,42 @@ const styles = {
     width:'100%',
   },  
   imgArea:{
-    width:'50%',
-    height:'629px',
-    backgroundColor:'blue',
+    width:'2500px',
+    minWidth:'200px',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',  
+    borderRight:'solid 3px #5a737d',
+  },
+  ImagenFondo:{
+    
+    paddingTop:'10px',
+    paddingBottom:'10px',
+    maxWidth:'780px',
+    minHeight:'555px',
+    minWidth:'300px'
   },
   formArea:{
-    width:'50%',
-    backgroundColor:'black',
+    minWidth:'358px',
+    width:'1360px',
+    maxWidth:'1500px',
     display:'flex',
     justifyContent:'center',
   },
   formContainer: {
-    marginTop:'10px',
-    marginBottom:'10px',
-    backgroundColor: "#cbd0ff",
+    backgroundColor: "#fafafa",
     borderRadius: 10,
     paddingLeft: '20px',
-    paddingRight:'42px',
+    paddingRight:'20px',
     width: '35%',
-    minWidth: '400px',
+    minWidth: '290px',
+  },
+  cincuenta:{
+    display:'flex',
+    justifyContent:'space-between'
+  },
+  inCincuenta:{
+    width:'45%',
   },
   title: {
     fontSize: 24,
@@ -108,7 +135,7 @@ const styles = {
     marginBottom: 5,
   },
   input: {
-    width: '100%',
+    width: '93%',
     backgroundColor: "#fff",
     borderColor: "#000",
     borderWidth: 1,
@@ -126,14 +153,15 @@ const styles = {
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
-    display: 'block',
     width: '100%',
-    marginVertical: 10,
   },
   loginButtonText: {
+    display:'flex',
+    justifyContent:'right',
     color: "blue",
     textAlign: "center",
-    marginTop: 10,
+    marginTop: 5,
+    paddingBottom:'10px',
     cursor: 'pointer',
     backgroundColor: 'transparent',
     border: 'none',
